acc430db0f85e4e6bf6fd7207e997e52ce22cfc3
Serge Bouchut <serge.bouchut@gmail.com>
2017-02-23 22:14:37 +0100

    update 2

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-import pdb
+import json
 
 
 class Data:
@@ -14,7 +14,7 @@ class Data:
                 self.count['endpoints'],
                 self.count['requests'],
                 self.count['cache'],
-                self.count['cache_sizes']
+                self.count['cache_size']
             ) = f.readline().split(' ')
             for k, v in self.count.items():
                 self.count[k] = int(v)
@@ -25,27 +25,86 @@ class Data:
                     'requests': {},
                 })
 
+            # self.endpoints = [{
+            #     'latency_to_datacenter': 1000,
+            #     'caches': {
+            #           '0': 100,
+            #           '1': 200,
+            #        }
+            # }]
+
             for endpoint in range(int(self.count['endpoints'])):
-                distance, cache_number = f.readline().split(' ')
+                latency_to_datacenter, cache_count = f.readline().split(' ')
                 caches = {}
-                for cache in range(int(cache_number)):
+                for cache in range(int(cache_count)):
                     cache_id, cache_distance = f.readline().split(' ')
-                    caches['id'] = cache_distance.replace("\n", "")
+                    caches[cache_id] = cache_distance.replace("\n", "")
                 self.endpoints.append({
-                    'distance': distance,
+                    'latency_to_datacenter': latency_to_datacenter,
                     'caches': caches,
                 })
 
             for request in range(int(self.count['requests'])):
-                video_id, endpoint_id, request_nb = f.readline().split(' ')
-                self.videos[int(video_id)]['requests'][int(endpoint_id)] = int(request_nb)
+                video_id, endpoint_id, request_count = f.readline().split(' ')
+                self.requests.append({
+                    'video_id': video_id,
+                    'endpoint_id': endpoint_id,
+                    'count': int(request_count),
+                })
+                self.videos[int(video_id)]['requests'][int(endpoint_id)] = int(request_count)
 
-    def calc_
+    def get_video_size(self, video_id):
+        return int(self.videos[int(video_id)]['size'])
 
-    def __str__(self):
-        return "count: " + str(self.count) + "\n\nendpoints: " + str(self.endpoints) + "\nrequests: " + str(self.requests)
+    def ponderate_cache(self):
+        self.caches = []
+        for cache_id in range(self.count['cache']):
+            self.caches.append({
+                'remaining_size': self.count['cache_size'],
+                'candidate_videos': {},
+                'assigned_videos': {},
+            })
+        for request in self.requests:
+            video_id = int(request['video_id'])
+            endpoint_id = int(request['endpoint_id'])
+            weight = request['count'] * self.get_video_size(video_id)
+            endpoint = self.endpoints[endpoint_id]
+            for cache_id, cache_latency in endpoint['caches'].items():
+                old_weight = self.caches[int(cache_id)]['candidate_videos'].get(video_id, 0)
+                self.caches[int(cache_id)]['candidate_videos'][video_id] = (
+                    (old_weight + weight) / int(cache_latency))
+
+    def get_most_requested_video(self, cache_id):
+        cache = self.caches[cache_id]
+        max_weight = 0
+        most_requested_video_id = 0
+        for video_id, weight in cache['candidate_videos'].items():
+            if int(weight) > max_weight:
+                print(int(weight), max_weight)
+                most_requested_video_id = video_id
+                max_weight = int(weight)
+        return most_requested_video_id
+
+    def assign_video_to_cache(self, video_id, cache_id):
+        video_size = self.get_video_size(video_id)
+        cache = self.caches[cache_id]
+        if video_size < cache['remaining_size']:
+            cache['assigned_videos'][video_id] = video_size
+            cache['candidate_videos'].pop(video_id)
+            cache['remaining_size'] -= video_size
+
+    def print_counts(self):
+        print(self.count)
+
+    def print_caches(self):
+        print(json.dumps(self.caches[:1], indent=4))
+
 
 if __name__ == "__main__":
     d = Data()
-
-    print(d)
+    d.ponderate_cache()
+    for trial in range(1):
+        for cache_id, cache in enumerate(d.caches[:1]):
+            video_id = d.get_most_requested_video(cache_id)
+            d.assign_video_to_cache(video_id, cache_id)
+    d.print_caches()
