04bc959fcd2341a1b0b762c9bc2b597cd9e0d521
Serge Bouchut <serge.bouchut@gmail.com>
2017-02-27 23:55:48 +0100

    Try with cumulated cost for all request for same video to endpoints sharing cache

--- data.py
+++ data.py
@@ -1,17 +1,20 @@
+from datetime import datetime
+
+
 class Cache:
     def __init__(self, id, size):
         self.id = id
         self.available_size = size
         self.assigned_videos = []
 
-    def assign_video(self, request):
+    def assign_video(self, request, caches):
         video = request.video
         # check video not already in cache
         if video.size > self.available_size or video.id in self.assigned_videos:
             return False
         # check video not already in another cache for the endpoint
-        for cache in request.endpoint.sorted_caches:
-            if video.id in cache.assigned_videos:
+        for cache_id in request.endpoint.sorted_caches:
+            if video.id in caches[cache_id].assigned_videos:
                 return False
         self.assigned_videos.append(video.id)
         self.available_size -= video.size
@@ -22,6 +25,7 @@ class Video:
     def __init__(self, id, size):
         self.id = id
         self.size = size
+        self.requests = []
 
 
 class Endpoint:
@@ -31,13 +35,13 @@ class Endpoint:
 
     def add_cache_latency(self, cache, latency):
         self.caches.append({
-            'cache': cache,
+            'id': cache.id,
             'gain': self.datacenter_latency - latency,
         })
 
     def sort_caches_by_latency(self):
         self.sorted_caches = [
-            item['cache'] for item in sorted(
+            item['id'] for item in sorted(
                 self.caches,
                 key=lambda item: item['gain'],
                 reverse=True,
@@ -48,11 +52,22 @@ class Endpoint:
 class Request:
     def __init__(self, video, endpoint, count):
         self.video = video
+        self.video.requests.append(self)
         self.endpoint = endpoint
         self.count = count
 
     def compute_cost(self):
         self.cost = self.count * self.video.size * self.endpoint.datacenter_latency
+
+    def compute_cumulated_cost(self):
+        self.cumulated_cost = 0
+        # add request cost on same video for other endpoint sharing cache
+        for request in self.video.requests:
+            if any([
+                cache_id in self.endpoint.sorted_caches
+                for cache_id in request.endpoint.sorted_caches
+            ]):
+                self.cumulated_cost += request.cost
 
 
 def input_reader(filename, caches, videos, endpoints, requests):
@@ -115,6 +130,8 @@ if __name__ == '__main__':
         'videos_worth_spreading',
         'kittens',
     ):
+        print(filename)
+
         # init data
         caches = []
         videos = []
@@ -128,17 +145,29 @@ if __name__ == '__main__':
         for request in requests:
             request.compute_cost()
 
+        # compute cumulated cost for each request
+        percent = len(requests) / 100
+        for index, request in enumerate(requests):
+            if index % percent == 0:
+                print(
+                    '%s --> %02d%%' % (
+                        datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                        index / percent,
+                    )
+                )
+            request.compute_cumulated_cost()
+
         # sort request from higher to lower cost
         sorted_requests = sorted(
             requests,
-            key=lambda request: request.cost,
+            key=lambda request: request.cumulated_cost,
             reverse=True,
         )
 
         # find best cache to assigned to video
         for request in sorted_requests:
-            for cache in request.endpoint.sorted_caches:
-                if cache.assign_video(request):
+            for cache_id in request.endpoint.sorted_caches:
+                if caches[cache_id].assign_video(request, caches):
                     break
 
         # write output data
